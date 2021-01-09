49a5431c6d0953159eb6c11a76ff43eb137fffd4
AstralWatcher <andrijacvejic@gmail.com>
2021-01-09 22:21:07 +0100

    React Native Navigation Part 3, home component updated to render 3 featured cards (leaders,promotions, dishes)

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,12 +1,59 @@
-import React, {Component} from 'react';
-import {View,Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+
+import {DISHES} from '../shared/dishes'
+import {PROMOTIONS} from '../shared/promotions'
+import {LEADERS} from '../shared/leaders'
+
+function RenderCard(props) {
+    const info = props.info;
+
+    if (info) {
+        const subText = (info.designation) ? info.designation : '';
+
+        const textStyle = { backgroundColor: 'white', color: 'black' };
+        const textStyleAligment = { flex: 1, alignItems: 'center', justifyContent: 'center' };
+        return (
+            <Card>
+                <Card.Image source={require('../images/alberto.png')}>
+                    <View style={textStyleAligment}>
+                        <Card.FeaturedTitle style={textStyle}>{info.name}</Card.FeaturedTitle>
+                        <Card.FeaturedSubtitle style={textStyle}>{subText}</Card.FeaturedSubtitle>
+                    </View>
+                </Card.Image>
+                <Text style={{ margin: 10 }}>
+                    {info.description}
+                </Text>
+            </Card>
+        );
+    } else {
+        return (<View></View>);
+    }
+
+}
+
 
 class Home extends Component {
 
-    render(){
-        return(
+    constructor(props) {
+        super(props);
+        this.state ={
+            dishes:DISHES,
+            leaders:LEADERS,
+            promotions:PROMOTIONS
+        }
+    }
+
+    render() {
+        return (
             <View>
                 <Text>Home component</Text>
+                <ScrollView>
+                    <RenderCard info={this.state.dishes.filter( item => item.featured === true)[0]} />
+                    <RenderCard info={this.state.promotions.filter( item => item.featured === true)[0]} />
+                    <RenderCard info={this.state.leaders.filter( item => item.featured === true)[0]} />
+                </ScrollView>
             </View>
         );
     }
