82faab18ddadec02fcc53d56c61ce2310f6248d9
AstralWatcher <andrijacvejic@gmail.com>
2021-02-15 19:34:23 +0100

    3.2.2 Added React Animated sliding animation for homeComponent that is sliding right to left cards

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, Animated, Easing, TouchableHighlight } from 'react-native';
 import { Card } from 'react-native-elements';
 
-import {connect} from 'react-redux';
-import {baseUrl} from '../shared/baseUrl';
-import {Loading} from './LoadingComponent';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -17,12 +17,12 @@ const mapStateToProps = state => {
 function RenderCard(props) {
     const info = props.info;
 
-    if(props.isLoading) {
+    if (props.isLoading) {
         return (
             <Loading />
         )
-    } else if(props.errMess) {
-        return(
+    } else if (props.errMess) {
+        return (
             <View>
                 <Text>{props.errMess}</Text>
             </View>
@@ -36,7 +36,7 @@ function RenderCard(props) {
         const textStyleAligment = { flex: 1, alignItems: 'center', justifyContent: 'center' };
         return (
             <Card>
-                <Card.Image source={ {uri:  info.image}}>
+                <Card.Image source={{ uri: info.image }}>
                     <View style={textStyleAligment}>
                         <Card.FeaturedTitle style={textStyle}>{info.name}</Card.FeaturedTitle>
                         <Card.FeaturedSubtitle style={textStyle}>{subText}</Card.FeaturedSubtitle>
@@ -56,16 +56,79 @@ function RenderCard(props) {
 
 class Home extends Component {
 
+    constructor(props) {
+        super(props);
+        this.animatedValue = new Animated.Value(0);
+        this.state = {
+            stopAnimation: false
+        }
+    }
+
+    componentDidMount() {
+        this.animate();
+    }
+
+    animate() {
+        this.animatedValue.setValue(0);
+        Animated.timing(
+            this.animatedValue,
+            {
+                toValue: 8,
+                duration: 8000,
+                useNativeDriver:true,
+                easing: Easing.linear
+            }
+        ).start(() => 
+        {
+            if(this.state.stopAnimation === false) {
+                this.animate();
+            }
+        }
+        )
+    }
+
+    _onPressButtonSlider(){
+        //TODO needs fixing for stopping animation
+        this.animatedValue.stopAnimation();
+        this.animatedValue.setValue(0);
+        this.setState({
+            stopAnimation: !this.state.stopAnimation
+        })
+        this.animate()
+    }
+
     render() {
+        const xpos1 = this.animatedValue.interpolate({
+            inputRange: [0, 1, 3, 5, 8],
+            outputRange: [600, 300, 0, -300, -600]
+        });
+        const xpos2 = this.animatedValue.interpolate({
+            inputRange: [0, 2, 4, 6, 8],
+            outputRange: [600, 300, 0, -300, -600]
+        });
+        const xpos3 = this.animatedValue.interpolate({
+            inputRange: [0, 3, 5, 7, 8],
+            outputRange: [600, 300, 0, -300, -600]
+        });
         return (
-            <View>
-                <Text>Home component</Text>
-                <ScrollView>
-                    <RenderCard errMess={this.props.dishes.errMess} isLoading={this.props.dishes.isLoading} info={this.props.dishes.dishes.filter( item => item.featured === true)[0]} />
-                    <RenderCard errMess={this.props.promotions.errMess} isLoading={this.props.promotions.isLoading} info={this.props.promotions.promotions.filter( item => item.featured === true)[0]} />
-                    <RenderCard errMess={this.props.leaders.errMess} isLoading={this.props.leaders.isLoading} info={this.props.leaders.leaders.filter( item => item.featured === true)[0]} />
-                </ScrollView>
-            </View>
+            <ScrollView>
+                <View>
+                    <Text>Home component</Text>
+                    <TouchableHighlight onPress={() => this._onPressButtonSlider} underlayColor="white">
+                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+                            <Animated.View style={{ width: '100%', transform: [{ translateX: xpos1 }] }}>
+                                <RenderCard errMess={this.props.dishes.errMess} isLoading={this.props.dishes.isLoading} info={this.props.dishes.dishes.filter(item => item.featured === true)[0]} />
+                            </Animated.View>
+                            <Animated.View style={{ width: '100%', transform: [{ translateX: xpos2 }] }}>
+                                <RenderCard errMess={this.props.promotions.errMess} isLoading={this.props.promotions.isLoading} info={this.props.promotions.promotions.filter(item => item.featured === true)[0]} />
+                            </Animated.View>
+                            <Animated.View style={{ width: '100%', transform: [{ translateX: xpos3 }] }}>
+                                <RenderCard errMess={this.props.leaders.errMess} isLoading={this.props.leaders.isLoading} info={this.props.leaders.leaders.filter(item => item.featured === true)[0]} />
+                            </Animated.View>
+                        </View>
+                    </TouchableHighlight>
+                </View>
+            </ScrollView>
         );
     }
 
