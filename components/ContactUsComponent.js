9a8047b1b8923cce2fa6603d4549fcc6a0f6d588
AstralWatcher <andrijacvejic@gmail.com>
2021-02-21 21:46:48 +0100

    4.4.2 Activating for sending email with pre filled fields via MailComposer from  expo-mail-composer

--- components/ContactUsComponent.js
+++ components/ContactUsComponent.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 class ContactUs extends Component {
 
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@balas.rs'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern,\n'
+        });
+    }
 
     render() {
         const info = ['Our Address 121', 'Clear Water Bay Road', 'Clear Water Bay Kowloon', 'HONG KONG', 'Tel: +852 1234 5678', 'Fax: +852 8765 4321', 'Email:confusion@food.net'];
@@ -18,6 +26,12 @@ class ContactUs extends Component {
                         <Card.Title>Contact Information</Card.Title>
                         <Card.Divider />
                         <Text>{render}</Text>
+                        <Button
+                            title=' Send Email'
+                            buttonStyle ={{backgroundColor: '#512DA8'}}
+                            icon={<Icon name='envelope-o' type='font-awesome' color='white'/>}
+                            onPress={()=>this.sendMail()}
+                         />
                     </Card>
                 </Animatable.View>
             </View>
